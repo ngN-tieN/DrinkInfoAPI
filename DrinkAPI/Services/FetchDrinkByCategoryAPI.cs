6d18d53fb5777dd9f35d21f6f346b3f98ada860f
tien <nq2017.nguyentien081202@gmail.com>
2024-05-07 18:49:05 +0700

    Add methods to fetchDrinks'Info by category name

---
+++ DrinkAPI/Services/FetchDrinkByCategoryAPI.cs
@@ -0,0 +1,29 @@
+﻿using DrinkAPI.Model;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace DrinkAPI.Services
+{
+    internal class FetchDrinksByCategoryAPI
+    {
+        public async Task<List<DrinkDTO>> FetchDrinksByCategory(string categoryName)
+        {
+            using(HttpClient client = new()) 
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                string uri = $"https://www.thecocktaildb.com/api/json/v1/1/filter.php?c={categoryName}";
+                var drinkList = await ProcessDrinksByCategoryAsync(client, uri);
+                return drinkList;
+            }
+        }
+
+        public async Task<List<DrinkDTO>> ProcessDrinksByCategoryAsync(HttpClient client, string api) 
+        {
+            var stream = await client.GetStreamAsync(api);
+            var drinks =
+                await JsonSerializer.DeserializeAsync<DrinksInfo>(stream);
+            return drinks.Drinks;
+        }
+    }
+}
