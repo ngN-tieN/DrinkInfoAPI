86fe2c43670e07f91d334888bee0298e846b4f97
tien <nq2017.nguyentien081202@gmail.com>
2024-05-07 18:47:36 +0700

    Add Drinks'info model

---
+++ DrinkAPI/Model/Drinks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DrinkAPI.Model
+{
+    internal class DrinksInfo
+    {
+        [property:JsonPropertyName("drinks")]
+        public List<DrinkDTO> Drinks {  get; set; }
+    }
+}
