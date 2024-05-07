23b28490f886fb4be386553ee87ac869389d506a
tien <nq2017.nguyentien081202@gmail.com>
2024-05-07 18:37:30 +0700

    Add a model for Drink's info

---
+++ DrinkAPI/Model/Drink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DrinkAPI.Model
+{
+    internal class DrinkDTO
+    {
+        [property: JsonPropertyName("strDrink")]
+        public int Drink{ get; set; }
+
+        [property:JsonPropertyName("strDrinkThumb")] 
+        public Uri DrinkThumb { get; set; }
+
+        [property:JsonPropertyName("idDrink")]
+        public int Id { get; set;}
+    }
+}
