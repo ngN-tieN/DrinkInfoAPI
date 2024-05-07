6f8917bfbb18720ee452057b2bb480af789dfedf
tien <nq2017.nguyentien081202@gmail.com>
2024-05-07 18:36:56 +0700

    Add a model for category

---
+++ DrinkAPI/Model/Category.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace DrinkAPI.Models
+{ 
+    internal class CategoryDTO
+    {
+        [property: JsonPropertyName("strCategory")]
+        public string Category { get; set; } 
+    }
+}
