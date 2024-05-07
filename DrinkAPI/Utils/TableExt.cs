63e311800ea46e7c7afdb99ea049a5d91f795986
tien <nq2017.nguyentien081202@gmail.com>
2024-05-07 18:50:43 +0700

    Init file for creating methods to print report table

---
+++ DrinkAPI/Utils/TableExt.cs
@@ -0,0 +1,13 @@
+﻿using Spectre.Console;
+
+namespace DrinkAPI.Utils
+{
+    internal class TableExt
+    {
+        public static void PrintTable<T>(List<T> tabledata)
+            where T : class
+        {
+            
+        }
+    }
+}
