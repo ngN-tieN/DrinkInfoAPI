ff8d469c63df627c4c2caf0240823866644c2204
tien <nq2017.nguyentien081202@gmail.com>
2024-05-07 18:48:25 +0700

    Add a methods to get data from CategoryAPI

---
+++ DrinkAPI/Services/FetchCategoryAPI.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+using DrinkAPI.Model;
+using DrinkAPI.Models;
+namespace DrinkAPI.Services
+
+{
+    internal class FetchCategoryAPI
+    {
+        public async Task<List<CategoryDTO>> FetchCategories()
+        {
+            using (HttpClient client = new())
+            { 
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var categories = await ProcessCategoriesAsync(client, "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list");
+                return categories;
+            }
+        }
+        public async Task<List<CategoryDTO>> ProcessCategoriesAsync(HttpClient client, string api)
+        {
+            var stream = await client.GetStreamAsync(api);
+            var categories =
+                await JsonSerializer.DeserializeAsync<CategoriesDTO>(stream);
+            return categories.Categories;
+            
+        }
+
+    }
+}
