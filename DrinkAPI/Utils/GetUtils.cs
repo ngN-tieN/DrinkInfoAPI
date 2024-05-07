731296ba78501f01a76df49efd5264635ab047d4
tien <nq2017.nguyentien081202@gmail.com>
2024-05-07 18:49:57 +0700

    Init utility for getting things with message and validator

---
+++ DrinkAPI/Utils/GetUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkAPI.Utils
+{
+    internal class GetUtils
+    {
+        public string GetString(string msg)
+        {
+            Console.WriteLine(msg);
+            return Console.ReadLine();
+        }
+    }
+}
