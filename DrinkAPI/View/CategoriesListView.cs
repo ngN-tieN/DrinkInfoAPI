df8c3d9841595c5f747b16ee8d5dae4c322fbe42
tien <nq2017.nguyentien081202@gmail.com>
2024-05-08 18:04:08 +0700

    Add View folder and CategoriesListView; Add method DisplayCategories

---
+++ DrinkAPI/View/CategoriesListView.cs
@@ -0,0 +1,19 @@
+﻿using DrinkAPI.Model;
+using DrinkAPI.Models;
+using DrinkAPI.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkAPI.View
+{
+    internal class CategoriesListView
+    {
+        public void DisplayCategories(List<CategoryDTO> categories)
+        {
+            TableExt.PrintCategoriesTable(categories);
+        }
+    }
+}
